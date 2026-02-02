a31bc5fec033803fcdfa0ab6c695809629af0d89
Nguyễn Trần Gia Kỳ <156431153+gkynajru@users.noreply.github.com>
2026-02-02 14:35:29 +0000

    fix empty vector store on OpenAI playground

--- app/openai/uploader.py
+++ app/openai/uploader.py
@@ -1,15 +1,18 @@
-from app.openai.client import client
+from openai import OpenAI
+
+client = OpenAI()
 
 def upload_files(vector_store_id: str, paths: list):
     file_ids = []
+
     for p in paths:
-        f = client.files.create(
+        uploaded = client.files.create(
             file=open(p, "rb"),
             purpose="assistants"
         )
-        file_ids.append(f.id)
+        file_ids.append(uploaded.id)
 
-    client.vector_stores.file_batches.create(
+    client.vector_stores.files.batch_create(
         vector_store_id=vector_store_id,
         file_ids=file_ids
     )
